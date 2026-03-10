e4dbd0f4c7cbad6d5643ea5faed32b6da678c18f
AVF-MLINE <andreqfnn@gmail.com>
2026-03-10 19:38:22 +0300

    Remove Cloudflare-specific config

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "127.0.0.1",
-    allowedHosts: ["demo.m-line.space"]
-  },
-  preview: {
-    host: "127.0.0.1",
-    allowedHosts: ["demo.m-line.space"]
-  }
+  plugins: [react()]
 });
